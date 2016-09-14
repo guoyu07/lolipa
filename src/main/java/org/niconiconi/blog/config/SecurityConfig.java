8bae79611ea3dc233a2bac4bb9c6f7805a71925a
Volio <volio.liu@gmail.com>
2016-09-14 12:53:44 +0800

    Fixed encoding error

--- src/main/java/org/niconiconi/blog/config/SecurityConfig.java
+++ src/main/java/org/niconiconi/blog/config/SecurityConfig.java
@@ -9,7 +9,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
+import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 import javax.sql.DataSource;
 
@@ -36,6 +38,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+        http.addFilterBefore(filter,CsrfFilter.class);
+
         http.authorizeRequests()
                 .antMatchers("/admin/**")
                 .hasAnyAuthority("ADMIN").anyRequest().permitAll().and()
