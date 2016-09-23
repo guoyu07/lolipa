eefcf57b9b6172a5ede3433d68f0b93fa31f25ff
Volio <volio.liu@gmail.com>
2016-09-23 21:57:32 +0800

    Add database structure

---
+++ database.sql
@@ -0,0 +1,96 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+
+CREATE TABLE `articles` (
+  `id` bigint(20) NOT NULL,
+  `title` varchar(200) NOT NULL,
+  `content` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `comment_num` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `authorities` (
+  `username` varchar(100) NOT NULL,
+  `authority` varchar(100) NOT NULL DEFAULT 'ADMIN'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `authorities` (`username`, `authority`) VALUES
+('Admin', 'ADMIN');
+
+CREATE TABLE `comments` (
+  `coid` bigint(20) UNSIGNED NOT NULL,
+  `cid` bigint(20) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `author` varchar(200) NOT NULL,
+  `mail` varchar(200) NOT NULL,
+  `url` varchar(200) DEFAULT NULL,
+  `ip` varchar(64) DEFAULT NULL,
+  `agent` varchar(200) DEFAULT NULL,
+  `text` text,
+  `status` varchar(16) DEFAULT 'approved'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `pages` (
+  `id` int(11) NOT NULL,
+  `title` varchar(200) NOT NULL,
+  `slug` varchar(200) NOT NULL,
+  `content` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `pages` (`id`, `title`, `slug`, `content`, `created_at`) VALUES
+(1, '友情链接', 'links', '', NULL),
+(2, '关于', 'about', '', NULL);
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `enabled` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `users` (`id`, `username`, `password`, `enabled`) VALUES
+(1, 'Admin', '18f3b7a24f93ce8a7f216c80a2156ec13ad918fad931db22d35204d77b27d32ec7782558b2199a90', 1);
+
+
+ALTER TABLE `articles`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `authorities`
+  ADD PRIMARY KEY (`username`),
+  ADD UNIQUE KEY `authorities_username_uindex` (`username`);
+
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`coid`),
+  ADD KEY `cid` (`cid`);
+
+ALTER TABLE `pages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `slug` (`slug`(191));
+
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user_username_uindex` (`username`);
+
+
+ALTER TABLE `articles`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `comments`
+  MODIFY `coid` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+ALTER TABLE `pages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+ALTER TABLE `authorities`
+  ADD CONSTRAINT `authorities_user_username_fk` FOREIGN KEY (`username`) REFERENCES `users` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
